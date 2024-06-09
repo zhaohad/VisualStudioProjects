5af367e788006be55c45a8b4a15da0dedf43d935
zhaohad <zhaohad@gmail.com>
2024-06-09 12:42:36 +0800

    Update
    N/A
    
    Signed-off-by: zhaohad <zhaohad@gmail.com>

---
+++ 12864-IMG-BYTE/12864-IMG-BYTE/12864-IMG-BYTE.cpp
@@ -0,0 +1,58 @@
+﻿#include <opencv2/opencv.hpp>  
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+#include <opencv2/imgproc.hpp>  
+#include<iostream>  
+
+using namespace std;
+using namespace cv;
+
+#define LEN_12864_ROW 8
+#define LEN_12864_COL 128
+
+int main()
+{
+    freopen("./out.txt", "w", stdout);
+    Mat image = imread("eye128_64.png", IMREAD_GRAYSCALE);
+    // Mat image = imread("1.png", IMREAD_GRAYSCALE);
+    // Mat image = imread("Panda.jpg", IMREAD_GRAYSCALE);
+
+    int rows = image.rows;
+    int cols = image.cols;
+    printf("rows = %d, cols = %d\n", rows, cols);
+
+    uchar buf[LEN_12864_ROW][LEN_12864_COL];
+    memset(buf, 0, sizeof(buf));
+
+    for (int r = 0; r < rows; r++) {
+        uchar* sptr = image.ptr<uchar>(r);
+        for (int c = 0; c < cols; c++) {
+            uchar ch = sptr[c];
+            if (ch > (0xff / 2 * 1)) {
+                sptr[c] = 0;
+            }
+            else {
+                sptr[c] = 0xff;
+            }
+
+            char index = r / 8;
+            char bufc = buf[index][c];
+            if (sptr[c]) {
+                buf[index][c] |= (1 << (r % 8));
+            }
+        }
+    }
+
+    for (int r = 0; r < LEN_12864_ROW; r++) {
+        const uchar* sptr = buf[r];
+        for (int c = 0; c < LEN_12864_COL; c++) {
+            uchar ch = sptr[c];
+            printf("0x%x, ", ch);
+        }
+        printf("\n");
+    }
+
+    imshow("Show Window", image);
+    waitKey(0);
+    return 0;
+}
---
+++ ConsolePos/ConsolePos/ConsolePos.c
@@ -0,0 +1,107 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+// 设置光标位置的函数
+void setCursorPosition(int x, int y) {
+    // 获取控制台输出句柄
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // COORD结构体保存光标的新位置
+    COORD position;
+    position.X = x;
+    position.Y = y;
+
+    // 设置光标位置
+    SetConsoleCursorPosition(hConsole, position);
+}
+
+int b[4][4] = { 0, 0, 1, 0,
+               0, 1, 1, 1,
+               0, 0, 0, 0,
+               0, 0, 0, 0 };
+
+int b1[4][4] = { 0, 0, 1, 0,
+               0, 0, 1, 0,
+               0, 0, 1, 1,
+               0, 0, 0, 0 };
+
+int b2[4][4] = { 0, 1, 1, 0,
+               0, 0, 1, 1,
+               0, 0, 0, 0,
+               0, 0, 0, 0 };
+
+int main() {
+    // 清屏
+    int x = 10, y = 5;
+    system("cls");
+    setCursorPosition(0, 20);
+    printf("■□─━│┃┄┅┆┇┈┉┊┋┌┍┎┏┐┑┒┓└┕┖┗┘┙┚┛├┝┞┟┠┡┢┣┤┥┦┧┨┩┪┫┬┭┮┯┰┱┲┳┴┵┶┷┸┹┺┻┼┽┾┿╀╁╂╃╄╅╆╇╈╉╊╋═║╒╓╔╕╖╗╘╙╚╛╜╝╞╟╠╡╢╣╤╥╦╧╨╩╪╫╬▁▂▃▄▅▆▇█▉▊▋▌▍▎▏");
+
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            setCursorPosition(y + j * 2, x + i);
+            if (b[i][j]) {
+                printf("█▉");
+            } else {
+            }
+        }
+    }
+
+    x = 14, y = 5;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            setCursorPosition(y + j * 2, x + i);
+            if (b1[i][j]) {
+                printf("█▉");
+            }
+            else {
+            }
+        }
+    }
+
+    x = 18, y = 5;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            setCursorPosition(y + j * 2, x + i);
+            if (b2[i][j]) {
+                printf("█▉");
+            }
+            else {
+            }
+        }
+    }
+
+    // 在不同位置输出文本
+    setCursorPosition(10, 5);
+    printf("This text is at (10, 5)");
+
+    setCursorPosition(20, 10);
+    printf("This text is at (20, 10)");
+
+    setCursorPosition(0, 0);
+    printf("Back to (0, 0)");
+
+    setCursorPosition(0, 22);
+    printf("┌───────────┬──────────┐\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("├───────────┼──────────┤\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("│           │          │\n");
+    printf("└───────────┴──────────┘\n");
+    
+
+    return 0;
+}
